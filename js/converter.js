7ac646712088ba65d5420c39cbad66adf06e6925
John Danquah-Boateng <yjbdanquah@gmail.com>
2018-07-03 17:00:30 +0000

    initial commit

---
+++ js/converter.js
@@ -0,0 +1,92 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('sw.js').then(wkd => {
+            
+            console.log('Registered');
+        }).catch(err => {
+            console.log('failed to register');
+        });
+    });
+}
+
+
+function convert() {
+    //    console.log("here")
+    const amount = document.getElementById("amount").value;
+    const currencyFrom = document.getElementById("currency").value;
+    const currencyTo = document.getElementById("current").value;
+
+    const msg = document.getElementById("msg");
+   const s1 = document.getElementById("s1").selected;
+   const s2 = document.getElementById("s2").selected;
+
+    if (amount <= 0) {
+
+        msg.innerHTML = `Please enter valid amount`;
+        document.getElementById("answer").value = `0.00`;
+   } else if ((s1 == true) || (s2 == true)) {
+
+        msg.innerHTML = `<small>Please select currency</small>`;
+        document.getElementById("answer").value = `0.00`;
+    } else {
+       msg.innerHTML = `<small>Convert your currency now!</small>`;
+       GetAsync(amount, currencyFrom, currencyTo, callback);
+
+   }
+}
+
+function callback(data, amount, query) {
+    const currencyTo = document.getElementById("current").value;
+    const rate = data[query].toFixed(5);
+    const total = amount * rate;
+    document.getElementById("rate").innerHTML = `@<strong id="rColor">${rate}</strong>`;
+    document.getElementById("answer").value = `${currencyTo} ${total}`;
+}
+
+
+function GetAsync(amount, currencyFrom, currencyTo, callback) {
+    const query = `${currencyFrom}_${currencyTo}`;
+    const url = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`;
+	
+	fetch(url).then(
+	response => {
+		if(response.status == 200){
+			console.log('response', response);
+			response.json().then(data => {
+				console.log('data', data);
+				callback(data, amount, query);
+			}).catch(ex => {
+                toast();
+                document.getElementById("answer").value = `?`;
+                document.getElementById("rate").innerHTML = `@<strong id="rColor">?</strong>`;
+            });
+		}
+	}).catch(err => {
+		console.log(err);
+       
+	});
+}
+   
+function toast() {
+    var x = document.getElementById("toastbar");
+    x.className = "show";
+    setTimeout(() => { x.className = x.className.replace("show", ""); }, 3000);
+}
+
+    
+    
+
+var dbPromise = idb.open('c-convertor', 1, (upgradeDb) => {
+    var keyValStore = upgradeDb.createObjectStore('exchange-rates');
+  });
+  
+  
+  // set"
+  function setVal(key,val){
+   dbPromise.then((db) =>{
+    var tx = db.transaction('exchange-rates', 'readwrite');
+    var keyValStore = tx.objectStore('exchange-rates');
+    keyValStore.put(val, key);
+    return tx.complete;
+  });
+  }
---
+++ sw.js
@@ -0,0 +1,79 @@
+const staticCacheName = 'version-4';
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            return cache.addAll([
+       '/',
+        '/index.html',
+        'css/converter.css',
+        'js/indexdb.js',
+        '/img',
+        '/img/converter-512.png',
+        '/css/bootstrap.min.css',
+        '/js/converter.js',
+        'https://free.currencyconverterapi.com/api/v5/currencies'
+      ])
+                .then(() => self.skipWaiting());
+
+        })
+    );
+});
+//
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+           return Promise.all(
+            cacheNames.filter(function(cacheName) {
+                return cacheName.startsWith('version-') && cacheName != staticCacheName; 
+            }).map(function(cacheName) {
+                return caches.delete(cacheName);                
+            })
+            );
+        })
+    );
+});
+
+
+//self.addEventListener('fetch', event => {
+//  event.respondWith(
+//    caches.open(cacheName)
+//      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+//      .then(response => {
+//      return response || fetch(event.request);
+//    })
+//  );
+//});
+
+
+addEventListener('fetch', function (event) {
+    
+    const requestUrl = new URL(event.request.url);
+    if (requestUrl.origin === location.origin) {
+        if (requestUrl.pathname === '/') {
+            event.respondWith(caches.match('/'));
+            return;
+        }
+    }
+        
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                return response; // if valid response is found in cache return it
+            } else {
+                return fetch(event.request).then(function (res) {
+                    return caches.open(staticCacheName)
+                        .then(function (cache) {
+                            cache.put(event.request.url, res.clone()); //save the response for future
+                            return res; // return the fetched data
+                        })
+                }).catch(function (err) {
+                    return caches.open(staticCacheName)
+                        .then(function (cache) {
+                            return cache.match('/index.html');
+                        });
+                });
+
+            }
+        })
+    );
+});
